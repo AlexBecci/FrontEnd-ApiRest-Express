bbaef1b9305327edc598cdf0146fb093d9ccae0a
Alex <beccialex2002@gmail.com>
2023-07-26 11:08:41 -0300

    in procces test

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,51 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Products from './components/Products';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Products from "./components/Products";
+import Loading from "./components/Loading";
 
 function App() {
   const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('https://backend-con-express-l1c91bkfc-alexbecci.vercel.app/api/v1/products');
-            const data = await response.json();
-            setData(data);
-            console.log(data)
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://backend-con-express-l1c91bkfc-alexbecci.vercel.app/api/v1/products"
+      );
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    if (!data) {
-        return <div>Cargando...</div>;
-    }
-
+  if (!data) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <Products/>
-    </div>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {data.map((product) => {
+            return (
+              <>
+                <Products
+                  name={product.name}
+                  image={product.image}
+                  price={product.price}
+                />
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
-export default App
+export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+function Products(props) {
+    const { name,price } = props;
 
-function Products() {
   return (
-    <div>Products</div>
-  )
+   
+          <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+            <a className="block relative h-48 rounded overflow-hidden">
+              <img
+                alt="ecommerce"
+                className="object-cover object-center w-full h-full block"
+                src="https://dummyimage.com/420x260"
+              />
+            </a>
+            <div className="mt-4">
+              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                CATEGORY
+              </h3>
+              <h2 className="text-gray-900 title-font text-lg font-medium">
+                {name}
+              </h2>
+              <p className="mt-1">${price}</p>
+            </div>
+          </div>
+
+  );
 }
 
-export default Products
+export default Products;
